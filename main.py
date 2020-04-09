64599aced19c563bcdbc99036bbefc61efcdaa1d
CleanPegasus <arunk609@gmail.com>
2020-04-09 14:42:53 +0530

    Added Neural Style Transfer

--- main.py
+++ main.py
@@ -1,65 +1,162 @@
-import os
-import sys
-import random
-import math
 
 import numpy as np
-import matplotlib.pyplot as plt
-import skimage.io
+import time
 
-from mrcnn import utils
-import mrcnn.model as modellib
-from mrcnn import visualize
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.applications import vgg19
+from tensorflow.keras import backend as K
 
-ROOT_DIR = os.path.abspath("/")
+from scipy.optimize import fmin_l_bfgs_b
+from scipy.misc import imsave
 
-sys.path.append(ROOT_DIR)
+from mask import mask_image
 
-MODEL_DIR = "logs"
+BACKGROUND_PATH = 'background.jpg'
+IMAGE_PATH = 'sample.jpeg'
+STYLE_PATH = 'wave.jpg'
 
-from coco import CocoConfig
+masked_image = mask_image(IMAGE_PATH, BACKGROUND_PATH)
+width, height = masked_image.size
+image_height = 400
+image_width = int(width * image_height/height)
 
-COCO_MODEL_PATH = "mask_rcnn_coco.h5"
+def preprocess_image(image):
 
-class InferenceConfig(CocoConfig):
-    GPU_COUNT = 1
-    IMAGES_PER_GPU = 1
+    img = image.resize((image_height, image_height))
+    img = img_to_array(img)
+    img = np.expand_dims(img, axis = 0)
+    img = vgg19.preprocess_input(img)
 
-config = InferenceConfig()
-config.display()
+    return img
 
-model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+def deprocess_image(x):
+    
+    x[:, :, 0] += 103.939
+    x[:, :, 1] += 116.779
+    x[:, :, 2] += 123.68
+    
+    x = x[:, :, ::-1]
+    x = np.clip(x, 0, 255).astype('uint8')
+    
+    return x
+
+class Evaluate(object):
+    
+    def __init__(self):
+        
+        self.loss_value = None
+        self.grad_value = None
+    
+    def loss(self, x):
+        
+        assert self.loss_value is None
+        x = x.reshape((1, image_height, image_width, 3))
+        outs = fetch_loss_and_grads([x])
+        loss_value = outs[0]
+        grad_value = outs[1].flatten().astype('float64')
+        self.loss_value = loss_value
+        self.grad_value = grad_value
+        
+        return self.loss_value
+    
+    def grads(self, x):
+        
+        assert self.loss_value is not None
+        grad_value = np.copy(self.grad_value)
+        self.grad_value = None
+        self.loss_value = None
+        
+        return grad_value
+
+target_image = K.constant(preprocess_image(masked_image)
+style_image = load_img(STYLE_PATH, target_size = (image_height, image_width))
+style_ref_image = K.constant(preprocess_image(style_image))
+combined_image = K.placeholder((1, image_height, image_width, 3))
+
+input_tensor = K.concatenate([target_image, style_ref_image, combined_image], axis = 0)
+
+model = vgg19.VGG19(input_tensor = input_tensor, weights = 'imagenet', include_top = False)
+
+def content_loss(base, combination):
+    return K.sum(K.square(combination - base))
+
+def gram_matrix(x):
+    
+    features = K.batch_flatten(K.permute_dimensions(x, (2, 0, 1)))
+    gram = K.dot(features, K.transpose(features))
+    
+    return gram
 
 
-model.load_weights(COCO_MODEL_PATH, by_name=True)
+def style_loss(style, combination):
+    
+    S = gram_matrix(style)
+    C = gram_matrix(combination)
+    channels = 3
+    size = image_height * image_width
+    
+    return K.sum(K.square(S - C)) / (4.0 * (channels ** 2) * size ** 2)
 
-class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
-               'bus', 'train', 'truck', 'boat', 'traffic light',
-               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
-               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
-               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
-               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-               'kite', 'baseball bat', 'baseball glove', 'skateboard',
-               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
-               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
-               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
-               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
-               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
-               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
-               'teddy bear', 'hair drier', 'toothbrush']
+def total_variation_loss(x):
+    
+    a = K.square(
+                x[:, :image_height - 1, :image_width - 1, :] - 
+                x[:, 1:, :image_width - 1, :])
+    b = K.square(
+                x[:, :image_height - 1, :image_width - 1, :] - 
+                x[:, 1:, :image_height - 1, :])
+    
+    return K.sum(K.pow(a + b, 1.25))
 
-image = skimage.io.imread("sample.jpeg")
+outputs_dict = dict([(layer.name, layer.output) for layer in model.layers])
+content_layer = 'block5_conv2'
+style_layers = ['block1_conv1',
+                'block2_conv1',
+                'block3_conv1',
+                'block4_conv1',
+                'block5_conv1',]
+total_variation_weight = 1e-4
+style_weight = 1.0
+content_weight = 0.025
 
-results = model.detect([image], verbose = 1)
 
-mask_image = results[0]['masks']
-result_image = image*mask_image
+loss = K.variable(0.0)
+layer_features = outputs_dict[content_layer]
+target_image_features = layer_features[0, :, :, :]
+combination_features = layer_features[2, :, :, :]
 
-result_image = image*mask_image
+loss = loss + content_weight + content_loss(target_image_features, combination_features)
 
-background = skimage.io.imread('background.jpg')
-background = background[:result_image.shape[0], :result_image.shape[1]]
+for layer_name in style_layers:
+    
+    layer_features = outputs_dict[layer_name]
+    style_ref_features = layer_features[1, :, :, :]
+    combination_features = layer_features[2, :, :, :]
+    
+    sl = style_loss(style_ref_features, combination_features)
+    
+    loss = loss + (style_weight / len(style_layers)) * sl
+    
+loss = loss + total_variation_weight * total_variation_loss(combined_image)
 
-masked_background = background * np.bitwise_not(mask_image)
-net_result = masked_background + result_image
+grads = K.gradients(loss, combined_image)[0]
+fetch_loss_and_grads = K.function([combined_image], [loss, grads])
+
+
+evaluator = Evaluate()
+
+iterations = 2
+
+x = preprocess_image(target_path)
+x = x.flatten()
+
+for i in range(iterations):
+    
+    print("Iteration: ", i)
+    x, min_val, info = fmin_l_bfgs_b(evaluator.loss, x, fprime = evaluator.grads, maxfun = 20)
+    print("Loss: ", min_val)
+    img = x.copy().reshape((image_height, image_width, 3))
+    img = deprocess_image(img)
+    fname = "musk_waves" + "_at_iteration_%d.png" %i
+    imsave(fname, img)
+    print("Image Saved")
